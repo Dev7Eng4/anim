7ae55c2ecef01096379ba1f77bcbe3ec22666b24
Duong Bich <duongbich9n7@gmail.com>
2026-03-25 11:15:49 +0700

    update character onepiece

--- src/contents/slide-show/amime/onepiece-pirate-captain.ts
+++ src/contents/slide-show/amime/onepiece-pirate-captain.ts
@@ -1,120 +1,34 @@
 import { staticFile } from "remotion";
 
-export const data = [
-  {
-    topTitle: "Edward Newgate",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Whitebeard Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
-  {
-    topTitle: "Bartolomeo",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Barto Club",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
+export const data1 = [
   {
     topTitle: "Brook",
     topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
     bottomTitle: "Rumbar Pirates",
     bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
   },
-  {
-    topTitle: "Xebec",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Rocks Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
+
   {
     topTitle: "Buggy",
     topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
     bottomTitle: "Buggy Pirates",
     bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
   },
-  {
-    topTitle: "Kaido",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Beast Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
+
   {
     topTitle: "Fake Luffy",
     topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
     bottomTitle: "Fake Straw Hat Pirates",
     bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
   },
-  {
-    topTitle: "Trafalgar Law",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Heart Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
-  {
-    topTitle: "Boa Hancock",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Kuja Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
-  {
-    topTitle: "Gol D. Roger",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Roger Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
-  {
-    topTitle: "Fisher Tiger",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Sun Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
-  {
-    topTitle: "Hody Jones",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "New Fishman Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
-  {
-    topTitle: "Usopp",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Usopp Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
-  {
-    topTitle: "Kuro",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Black Cat Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
-  {
-    topTitle: "Luffy",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Straw Hat Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
+
   {
     topTitle: "Shiki",
     topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
     bottomTitle: "Golden Lion Pirates",
     bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
   },
-  {
-    topTitle: "Charlotte Linlin",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Big Mom Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
-  {
-    topTitle: "Marshall D. Teach",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Blackbeard Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
-  {
-    topTitle: "Arlong",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Arlong Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
+
   {
     topTitle: "Crocodile",
     topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
@@ -127,54 +41,14 @@ export const data = [
     bottomTitle: "Revolutionary Army",
     bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
   },
-  {
-    topTitle: "Bellamy",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Bellamy Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
-  {
-    topTitle: "Portgas D. Ace",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Spade Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
-  {
-    topTitle: "Doflamingo",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Donquixote Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
-  {
-    topTitle: "Eustass Kid",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Kid Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
-  {
-    topTitle: "Gecko Moria",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Thriller Bark Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
+
   {
     topTitle: "Foxy",
     topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
     bottomTitle: "Foxy Pirates",
     bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
   },
-  {
-    topTitle: "Jewerly Bonney",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Bonney Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
-  {
-    topTitle: "Shanks",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Red Hair Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
+
   {
     topTitle: "Basil Hawkins",
     topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
@@ -187,24 +61,14 @@ export const data = [
     bottomTitle: "Tontatta Pirates",
     bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
   },
-  {
-    topTitle: "Krieg",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Krieg Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
+
   {
     topTitle: "Apoo",
     topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
     bottomTitle: "On Air Pirates",
     bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
   },
-  {
-    topTitle: "Capone Bege",
-    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
-    bottomTitle: "Fire Tank Pirates",
-    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
-  },
+
   {
     topTitle: "Wapol",
     topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
@@ -241,10 +105,179 @@ export const data = [
     bottomTitle: "Brownbeard Pirates",
     bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
   },
+];
+
+export const data = [
+  // 🔥 HOOK MỞ ĐẦU: Giữ chân ngay giây đầu tiên bằng Meme & Sự ngầu lòi
   {
-    topTitle: "Crocodile & Mihawk",
+    topTitle: "Buggy, Crocodile & Mihawk",
     topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
     bottomTitle: "Cross Guild",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "God Usopp", // Sửa thành God Usopp cho hợp trend
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Usopp Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Arlong",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Arlong Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+
+  // 📉 GIẢM NHỊP 1: Các thế lực quen thuộc ở Grand Line
+  {
+    topTitle: "Kuro",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Black Cat Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Don Krieg",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Krieg Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Bellamy",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Bellamy Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Gecko Moria",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Thriller Bark Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Hody Jones",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "New Fishman Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+
+  // 💥 HOOK GIỮA VIDEO: Kéo lại sự chú ý bằng Fan-Favorites & Trend hiện tại
+  {
+    topTitle: "Portgas D. Ace",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Spade Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Trafalgar Law",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Heart Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Boa Hancock",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Kuja Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Jewelry Bonney", // Đang rất hot trong Manga
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Bonney Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Dorry & Brogy", // Chuẩn bị cho Elbaf Arc
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Giant Warrior Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+
+  // 📉 GIẢM NHỊP 2: Dàn Siêu Tân Tinh & Cựu Thất Vũ Hải ngầu lòi
+  {
+    topTitle: "Eustass Kid",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Kid Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Capone Bege",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Fire Tank Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Bartolomeo",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Barto Club",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Doflamingo",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Donquixote Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Fisher Tiger",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Sun Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+
+  {
+    topTitle: "Charlotte Linlin",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Big Mom Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Kaido",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Beast Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Marshall D. Teach",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Blackbeard Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Shanks",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Red Hair Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+
+  // 👑 TRÙM CUỐI: Huyền thoại và Sự bùng nổ
+  {
+    topTitle: "Xebec",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Rocks Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Edward Newgate",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Whitebeard Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Gol D. Roger",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Roger Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Luffy (Gear 5)", // Cụ thể hoá Gear 5 để đẩy cảm xúc
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "Straw Hat Pirates",
+    bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
+  },
+  {
+    topTitle: "Joy Boy",
+    topImageSrc: staticFile("anime/onepiece-pirate-captain/Luffy.jpg"),
+    bottomTitle: "The First Pirate",
     bottomImageSrc: staticFile("anime/onepiece-pirate-captain/Zoro.jpg"),
   },
 ];
